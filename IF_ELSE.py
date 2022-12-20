ce4d6b86fa45160474883721235c9be3f1ddf2d1
Candace Hollinger <csig1985@gmail.com>
2022-12-20 03:47:34 +0000

    updated if else

---
+++ IF_ELSE.py
@@ -0,0 +1,53 @@
+
+if 'a' < 'b':
+    ...    print("CONDITION was true")
+CONDITION was true
+
+if 'b' < 'a':
+...    print("CONDITION was true") 
+...
+
+if False:
+...    print("Was true")
+... else:
+...    print("Was false")
+...
+Was false
+
+if True:
+...    print("Was true")
+... else:
+...    print("Was false")
+...
+Was true
+
+
+if 'b' < 'a'
+...    print("This is true")
+... elif 'c' < 'd':
+...    print("Second condition is true")
+... else:
+...    print("no condition was true")
+...
+Second condition is true
+
+
+name = input("What is your name? ")
+
+if len(name) >= 6:
+    print("Your name is long")
+elif len(name) == 5:
+    print("Your name is 5 characters")
+elif len(name) >= 4:
+    print("Your name is 4 or more characters")
+else:
+    print("Your name is short")
+
+
+
+name = "Candace"
+if name =="Candace"
+...    print("Hello Candace")
+... else:
+...    pass
+...
